c27e69c077a9fd8690e70595dcb48300736c675e
Mari Mekhashishvili <marimexashishvili8@gmail.com>
2024-01-21 16:26:34 +0400

    design patterns

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,48 +20,48 @@ const Services = () => {
         <span>იუსტიციის სახლის მიზანია მომხმარებლისათვის სახელმწიფო და კერძო სექტორის სერვისების მაღალი ხარისხით მიწოდება</span>
       </div>
      
-      <div className="flex mt-14 p-[16px] h-[60px]  flex-col justify-center items-center  border-radius-24 bg-opacity-50 bg-white ">
+      <div className="flex mt-14  h-[60px] w-[1194px]  flex-col justify-center items-center  border-radius-24 bg-opacity-50 bg-white ">
   <div className="flex p-[4px] h-[52px] items-start self-stretch rounded-3xl bg-white ">
     
     <ul className="flex justify-between">
-    <li className="flex max-w-360px p-[10px] justify-center items-center  flex-1 border-radius-22px ml-16 hover:bg-gray-300 active:bg-green-500 rounded-lg">
+    <li className="flex justify-center items-center w-[180px] rounded-3xl hover:bg-gray-300 active:bg-lime-300">
         <img src={personIcon} alt="Person Icon" className="h-6 w-6" />
-        <a href="#" className="text-black font-normal text-16 uppercase leading-115.5 tracking-wide">
+        <a href="#" className="text-black font-normal text-16 ml-[8px]  ">
           პიროვნება
         </a>
       </li>
-      <li className="flex max-w-360px p-[10px] justify-center items-center  flex-1 border-radius-22px ml-16 hover:bg-gray-300 active:bg-green-500 rounded-lg">
+      <li className="flex p-[10px] justify-center items-center rounded-3xl w-[180px] hover:bg-gray-300 active:bg-lime-300">
         <img src={businessIcon} alt="Business Icon" className="h-6 w-6" />
-        <a href="#" className="text-black font-normal text-16 uppercase leading-115.5 tracking-wide">
+        <a href="#" className="text-black font-normal text-16 ml-[8px]  ">
           ბიზნესი
         </a>
       </li>
-      <li className="flex max-w-360px p-[10px] justify-center items-center  flex-1 border-radius-22px ml-16 hover:bg-gray-300 active:bg-green-500 rounded-lg">
+      <li className="flex p-[10px] justify-center items-center rounded-3xl w-[180px] hover:bg-gray-300 active:bg-lime-300 ">
         <img src={propertyIcon} alt="Property Icon" className="h-6 w-6" />
-        <a href="#" className="text-black font-normal  text-16 uppercase leading-115.5 tracking-wide">
+        <a href="#" className="text-black font-normal  text-16  ml-[8px]  ">
           ქონება
         </a>
       </li>
-      <li className="flex max-w-360px p-[10px] justify-center items-center  flex-1 border-radius-22px ml-16 hover:bg-gray-300 active:bg-green-500 rounded-lg">
+      <li className="flex p-[10px] w-[180px] justify-center rounded-3xl items-center hover:bg-gray-300 active:bg-lime-300  ">
         <img src={enforcementIcon} alt="Enforcement Icon" className="h-6 w-6" />
-        <a href="#" className="text-black font-normal  text-16 uppercase leading-115.5 tracking-wide">
+        <a href="#" className="text-black font-normal  text-16 ml-[8px] ">
           აღსრულება
         </a>
       </li>
-      <li className="flex max-w-360px p-[10px] justify-center items-center flex-1 border-radius-22px ml-16 hover:bg-gray-300 active:bg-green-500  rounded-lg">
+      <li className="flex p-[10px] w-[180px] justify-center rounded-3xl items-center  hover:bg-gray-300 active:bg-lime-300">
         <img src={archiveIcon} alt="Archive Icon" className="h-6 w-6" />
-        <a href="#" className="text-black font-normal  text-16 uppercase leading-115.5 tracking-wide">
+        <a href="#" className="text-black font-normal  text-16 ml-[8px]  ">
           არქივი
         </a>
       </li>
-      <li className="flex max-w-360px p-[10px] justify-center items-center  flex-1 border-radius-22px ml-16 hover:bg-gray-300  active:bg-green-500 rounded-lg">
+      <li className="flex w-[180px] p-[10px] items-center rounded-3xl justify-center hover:bg-gray-300 active:bg-lime-300 ">
         <img src={networkIcon} alt="Network Icon" className="h-6 w-6" />
-        <a href="#" className="text-black font-normal  text-16 uppercase leading-115.5 tracking-wide">
+        <a href="#" className="text-black font-normal ml-[8px] text-16  ">
           ონალაინ
         </a>
       </li>
-      <li className="flex max-w-360px p-[10px] justify-center items-center  flex-1 border-radius-22px ml-16 hover:bg-gray-300 active:bg-green-500 rounded-lg">
-      <a href="#" className="text-black font-normal  text-16 uppercase leading-115.5 tracking-wide">
+      <li className="flex p-[10px] w-[100px] justify-center rounded-3xl items-center hover:bg-gray-300 active:bg-lime-300">
+      <a href="#" className="text-black font-normal ml-[8px] text-16  ">
     მეტი
   </a>
   <img src={ArrowRightIcon} alt="arrow right" className="h-6 w-6 "/>
